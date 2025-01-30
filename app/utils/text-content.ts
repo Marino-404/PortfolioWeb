e5d928df737e73f1a9ccf87801fae95df5c1488c
Juan Marino Rova <maarino89@hotmail.com>
2025-01-30 00:13:45 -0300

    1.0.5

--- app/utils/text-content.ts
+++ app/utils/text-content.ts
@@ -3,7 +3,7 @@ export const aboutTextContent = (lang: boolean) => {
     if (lang) {
       return {
         h1: "Juan",
-        h2: "Bienvenidos, yo soy",
+        h2: "Bienvenidos, soy",
       };
     } else {
       return {
