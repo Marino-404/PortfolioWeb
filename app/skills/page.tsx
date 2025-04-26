6c2fc27faaf48fb2546b74ad51c3b87dafa9777c
Marino-404 <marinorova25@gmail.com>
2025-04-26 14:04:55 -0300

    Producción

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -18,49 +18,36 @@ export default function SkillsComponent() {
     >
       <div className="flex flex-col md:flex-row items-center justify-center py-16 px-6 max-w-7xl w-full gap-4 md:gap-6">
         {/* Texto */}
-        <motion.div
-          className="text-center md:text-left basis-auto"
-          initial="hidden"
-          animate="visible"
-          variants={{
-            hidden: {},
-            visible: { transition: { staggerChildren: 0.2 } },
-          }}
+        <motion.h1
+          className="text-3xl md:text-4xl xl:text-5xl sm:text-5xl font-light
+          leading-tight md:leading-snug xl:leading-snug"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
         >
-          <motion.h1
-            className="text-3xl md:text-4xl sm:text-5xl font-light leading-tight md:leading-snug"
-            variants={{
-              hidden: { opacity: 0, y: -30 },
-              visible: {
-                opacity: 1,
-                y: 0,
-                transition: { duration: 0.8, ease: "easeOut" },
-              },
-            }}
+          {h1Parts[0]}
+          <span> {h1Parts[1]}</span> <br />
+          <span
+            className={`font-bold bg-gradient-to-b ${
+              theme
+                ? "from-[#023535] to-[#008F8C]"
+                : "from-[#008F8C] to-[#023535]"
+            } bg-clip-text text-transparent`}
           >
-            {h1Parts[0]} {h1Parts[1]} <br />
-            <span
-              className={`font-bold bg-gradient-to-b ${
-                theme
-                  ? "from-[#023535] to-[#008F8C]"
-                  : "from-[#008F8C] to-[#023535]"
-              } bg-clip-text text-transparent`}
-            >
-              {h1Parts[2]}
-            </span>{" "}
-            {h1Parts[3]} <br />
-            {h1Parts[4]} <br />
-            <span
-              className={`font-bold bg-gradient-to-b ${
-                theme
-                  ? "from-[#023535] to-[#008F8C]"
-                  : "from-[#008F8C] to-[#023535]"
-              } bg-clip-text text-transparent`}
-            >
-              {h1Parts[5]}
-            </span>
-          </motion.h1>
-        </motion.div>
+            {h1Parts[2]}
+          </span>{" "}
+          {h1Parts[3]} <br />
+          <span> {h1Parts[4]}</span> <br />
+          <span
+            className={`font-bold bg-gradient-to-b ${
+              theme
+                ? "from-[#023535] to-[#008F8C]"
+                : "from-[#008F8C] to-[#023535]"
+            } bg-clip-text text-transparent`}
+          >
+            {h1Parts[5]}
+          </span>
+        </motion.h1>
 
         {/* Imagen */}
         <motion.div
@@ -74,7 +61,7 @@ export default function SkillsComponent() {
             alt="Mockup Design"
             width={800}
             height={800}
-            className="w-[500px] sm:w-[400px] md:w-[500px] object-contain"
+            className="w-[500px] sm:w-[400px] md:w-[500px] xl:w-[500px] object-contain"
           />
         </motion.div>
       </div>
