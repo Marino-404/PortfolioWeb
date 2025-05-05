f2ff9a4b06181b7da6bfa289f8aebf6ffa041c39
Marino-404 <marinorova25@gmail.com>
2025-05-05 18:58:05 -0300

    Producción

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useAppState } from "../store/app-state";
 import { skillsTextContent } from "../utils/text-content";
 import { containerStyle, tittleStyle } from "../components/styles";
+import { motion } from "framer-motion";
 
 export default function SkillsComponent() {
   const { lang } = useAppState((state) => state);
@@ -12,7 +13,14 @@ export default function SkillsComponent() {
   return (
     <div className={containerStyle}>
       <div className="flex flex-col h-auto w-auto items-start justify-start p-4 min-h-[350px]">
-        <h1 className={tittleStyle}> {textContent.h2} </h1>
+        <motion.h1
+          className={tittleStyle}
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1.2 }}
+        >
+          {textContent.h2}
+        </motion.h1>
       </div>
     </div>
   );
